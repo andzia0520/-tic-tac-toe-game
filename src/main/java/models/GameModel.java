9c997799883091802742be411a64d595336ccb07
andzia0520 <ankadaniluk@gmail.com>
2020-07-16 18:06:42 +0200

    first commit

---
+++ src/main/java/models/GameModel.java
@@ -0,0 +1,17 @@
+package models;
+
+public class GameModel {
+    private char sign;
+
+    public GameModel(char sign) {
+        this.sign = sign;
+    }
+
+    public char getSign() {
+        return sign;
+    }
+
+    public void setSign(char sign) {
+        this.sign = sign;
+    }
+}
