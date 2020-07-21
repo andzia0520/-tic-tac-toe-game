ae6750319ccb8cb0f6798dc432896fa1516e92c8
andzia0520 <ankadaniluk@gmail.com>
2020-07-21 13:59:10 +0200

    step2

--- kodilla-individual-project/src/main/java/com/kodilla/game/TicTacToe.java
+++ kodilla-individual-project/src/main/java/com/kodilla/game/TicTacToe.java
@@ -1,15 +1,23 @@
 package com.kodilla.game;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
 
-    private Image imageback = new Image("file/scene.jpg");
+    private final Image imageback = new Image("file/scene.jpg");
+    private Image cross = new Image("file/cross.jpg");
+    private Image circle = new Image("file/circle.png");
+
+    private FlowPane signs = new FlowPane(Orientation.HORIZONTAL);
 
     public static void main(String[] args) {
         launch(args);
@@ -23,7 +31,52 @@ public class TicTacToe extends Application {
         Background background = new Background(backgroundImage);
 
         GridPane grid = new GridPane();
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.setHgap(17);
+        grid.setVgap(17);
         grid.setBackground(background);
+
+        ImageView img = new ImageView(cross);
+        //signs.getChildren().add(img);
+
+        ImageView img2 = new ImageView(cross);
+        //signs.getChildren().add(img2);
+
+        ImageView img3 = new ImageView(cross);
+        //signs.getChildren().add(img3);
+
+        ImageView img4 = new ImageView(circle);
+        //signs.getChildren().add(img4);
+
+        ImageView img5 = new ImageView(circle);
+        //signs.getChildren().add(img5);
+
+        ImageView img6 = new ImageView(circle);
+        //signs.getChildren().add(img6);
+
+        ImageView img7 = new ImageView(cross);
+        //signs.getChildren().add(img7);
+
+        ImageView img8 = new ImageView(circle);
+        //signs.getChildren().add(img8);
+
+        ImageView img9 = new ImageView(cross);
+        //signs.getChildren().add(img9);
+
+        grid.add(img,5,4,1,1);
+        grid.add(img2,7,4,1,1);
+        grid.add(img3,9,4,1,1);
+        grid.add(img4,5,7,1,1);
+        grid.add(img5,7,7,1,1);
+        grid.add(img6,9,7,1,1);
+        grid.add(img7,5,10,1,1);
+        grid.add(img8,7,10,1,1);
+        grid.add(img9,9,10,1,1);
+
+
+        //grid.setAlignment(Pos.TOP_LEFT);
+        //grid.addRow(10, img, img2);
+        grid.setHgap(20);
 
         Scene scene = new Scene((Parent) grid, 500, 500);
 
